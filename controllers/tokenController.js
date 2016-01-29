2288b2202109084f1b9828b2372356b57e686e1d
Karam Byun <byunkaram@gmail.com>
2016-01-28 20:00:54 -0500

    working angular to server

--- controllers/tokenController.js
+++ controllers/tokenController.js
@@ -6,10 +6,17 @@ myApp.controller('tokenCtrl',['$scope','$http',function($scope, $http){
   var refresh = function(){
     $http.get('/tokenlist').success(function(response){
       console.log("Received TOKENS DATA requested!");
-      $scope.tokenslist = response;
+      $scope.tokenlist = response;
       $scope.token = "";
     });
   }
   refresh();
+
+  $scope.addToken = function(){
+    console.log($scope.token);
+    $http.post('/tokenlist', $scope.token).success(function(response){
+      
+    });
+  }
 
 }]);
