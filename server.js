86b57df98df7f2a98b0a8b4ec5a0f052d64a5921
Karam Byun <byunkaram@gmail.com>
2016-01-28 23:39:50 -0500

    styling ui

--- server.js
+++ server.js
@@ -4,34 +4,34 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-// mongoose.connect('mongodb://localhost/ifscenario');
-// var db = mongoose.connection;
-// db.on('error',console.error.bind(console, 'connection error: '));
-// db.on('open', function(){
-//    console.log('We are in');
-//  });
+mongoose.connect('mongodb://localhost/ifscenario');
+var db = mongoose.connection;
+db.on('error',console.error.bind(console, 'connection error: '));
+db.on('open', function(){
+   console.log('We are in');
+ });
 
 /////////// FOR HEROKU ////////////////////////////////////////////////////////
 // Here we find an appropriate database to connect to, defaulting to
 // localhost if we don't find one.
-var uristring =
-  process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
-  'mongodb://localhost/HelloMongoose';
-
-// The http server will listen to an appropriate port, or default to
-// port 5000.
-var theport = process.env.PORT || 5000;
-
-// Makes connection asynchronously.  Mongoose will queue up database
-// operations and release them when the connection is complete.
-mongoose.connect(uristring, function (err, res) {
-  if (err) {
-    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-  } else {
-    console.log ('Succeeded connected to: ' + uristring);
-  }
-});
+// var uristring =
+//   process.env.MONGOLAB_URI ||
+//   process.env.MONGOHQ_URL ||
+//   'mongodb://localhost/HelloMongoose';
+//
+// // The http server will listen to an appropriate port, or default to
+// // port 5000.
+// var theport = process.env.PORT || 5000;
+//
+// // Makes connection asynchronously.  Mongoose will queue up database
+// // operations and release them when the connection is complete.
+// mongoose.connect(uristring, function (err, res) {
+//   if (err) {
+//     console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+//   } else {
+//     console.log ('Succeeded connected to: ' + uristring);
+//   }
+// });
 ///////////////////////////////////////////////////////////////////////////////
 
 //go into public find static index.html (or any)
@@ -83,6 +83,6 @@ app.put('/tokenlist/:id', function(req,res){
   });
 });
 
-app.listen(theport);
-//app.listen(3001);
+//app.listen(theport);
+app.listen(3001);
 console.log('Server Running on Port 5000');
